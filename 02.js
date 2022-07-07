cdabb7936c356d8ac1d53b750f56db9251f4062e
Denis Klopat <denisklopot0@gmail.com>
2022-07-07 19:08:21 +0300

    delete property

--- 02.js
+++ 02.js
@@ -1,1 +1,9 @@
-const name = "Tripp";
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"],
+    "bark": "woof"
+};
+
+delete myDog.tails;
